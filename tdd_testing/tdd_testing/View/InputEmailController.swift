2ccb721a75aff17ace67b971aa0333dd161151d5
Hao Tran Thien <mr.tranthienhao@gmail.com>
2020-08-22 00:51:34 +0700

    feat(init): initial commit

---
+++ tdd_testing/tdd_testing/View/InputEmailController.swift
@@ -0,0 +1,57 @@
+//
+//  InputEmailController.swift
+//  tdd_testing
+//
+//  Created by Tran Thien Hao on 8/10/20.
+//  Copyright © 2020 Tran Thien Hao. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class InputEmailController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    var inputEmailVM: InputEmailViewModelType!
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _setBtnEnability(isEnable: false)
+        setupBindings()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func setupBindings(){
+        emailTextfield.rx.text
+          .debounce(.seconds(2), scheduler: MainScheduler.instance)
+          .subscribe(onNext: { text in
+              self.inputEmailVM?.checkEmail(text!)
+          })
+          .disposed(by: disposeBag)
+
+        inputEmailVM?.isValidEmail
+          .observeOn(MainScheduler.instance)
+          .subscribe(onNext: {(value) in
+              self._setBtnEnability(isEnable: value)
+          }).disposed(by: disposeBag)
+    }
+    
+    func _setBtnEnability(isEnable: Bool = true) {
+        submitBtn.isEnabled = isEnable
+        submitBtn.alpha = isEnable ? 1 : 0.8
+    }
+    
+    @IBAction func navigateToOTPScreen(_ sender: Any) {
+        let  otpScreen = self.storyboard?.instantiateViewController(identifier: "InputOTPController") as! InputOTPController
+        otpScreen.mockOTP = "123456"
+        self.navigationController?.pushViewController(otpScreen, animated: true)
+    }
+    
+}
+
